b05e8b7c5e61ad651dbd97f79efe4f95b9b32a9b
vitalikost <vitalikostrovskij@yandex.ru>
2017-11-22 11:02:04 +0200

    add form edit,delete

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -1,31 +1,52 @@
 class QuestionController < ApplicationController
 
+  def edit
+    @question = Question.find(params[:id])
+  end
+
   def new
+    @question = Question.new
   end
 
   def create
     @question = Question.new(question_params)
-
     if @question.save
+      redirect_to @question
     else
       render "new"
     end
-    redirect_to @question
-
   end
 
   def show
-  #  render html:"Goods add"
-    @question = Question.find(params[:id])
+      @question = Question.find(params[:id])
   end
 
   def index
     @question = Question.all
   end
 
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render 'edit'
+    end
+  end
+
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+
+    redirect_to question_index_path
+  end
+
   private
   def question_params
     params.require(:question).permit(:title, :text)
   end
+
 
 end
