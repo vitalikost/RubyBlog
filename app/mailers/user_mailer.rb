b7d5bfa099efef3443fcc133f5f613858d8b0806
vitalik <vitalikostrovskij@yandex.ru>
2017-12-15 22:01:51 +0200

    Добавлена робота с мылом, регистрация и добавление ответа к вопросу

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,8 +4,15 @@ class UserMailer < ApplicationMailer
 
   def welcome_email
     @user = User.find(params[:user])
-    @url  = registration_url
+    @url  = session_login_path
     mail(to: @user.email, subject: 'Регистрация на сайте')
   end
 
+
+  def add_answers
+    @user = User.find(params[:user])
+    @question = Question.find(params[:question])
+    @url  = question_path(@question)
+    mail(to: @user.email, subject: 'Новый ответ на вопрос')
+  end
 end
